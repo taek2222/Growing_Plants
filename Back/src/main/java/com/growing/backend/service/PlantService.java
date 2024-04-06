b25fab2a3c43fe71707758f07ad9cf908a437ae8
taek2222 <oot309@daum.net>
2024-04-06 16:49:52 +0900

    [Oh] Test JPA, 엔드포인트 생성

---
+++ Back/src/main/java/com/growing/backend/service/PlantService.java
@@ -0,0 +1,27 @@
+package com.growing.backend.service;
+
+import com.growing.backend.dto.PlantDTO;
+import com.growing.backend.entity.Plant;
+import com.growing.backend.repository.PlantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PlantService {
+    private final PlantRepository plantRepository;
+
+    public List<PlantDTO> getPlantAllSearch() {
+        List<Plant> plants = plantRepository.findAll();
+        return plants.stream()
+                .map(plant -> {
+                    PlantDTO dto = new PlantDTO();
+                    dto.setPlantId(plant.getPlantId());
+                    dto.setPlantName(plant.getPlantName());
+                    return dto;
+                }).collect(Collectors.toList());
+    }
+}
