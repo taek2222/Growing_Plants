4080a919bd09efa2f5b254c91a1a29186371c5b5
taek2222 <oot309@daum.net>
2024-04-28 17:52:40 +0900

    [Oh] 날씨 DTO 생성

--- Back/src/main/java/com/growing/backend/dto/WeatherDataDTO.java
+++ Back/src/main/java/com/growing/backend/dto/WeatherDataDTO.java
@@ -7,8 +7,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class WeatherDataDTO {
-    private double minTemp;  // 최저 기온
+    private int day; // 오늘(1), 내일(2), 모레(3)
+    private int time; // 시간
     private double maxTemp;  // 최고 기온
-    private double currentTemp;  // 현재 기온
-    private int weatherCode;
+    private double minTemp;  // 최저 기온
+    private int currentTemp;  // (1시간) 기온
+    private int humidity; // 습도
+    private int weatherCode; // 날씨 상태 코드
+    private int rain; // 강수 확률
 }
