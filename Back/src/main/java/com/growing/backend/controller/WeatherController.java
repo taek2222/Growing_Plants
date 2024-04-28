e565cfec0bf399b7ac825ed5691f134c813d31ab
taek2222 <oot309@daum.net>
2024-04-29 00:37:38 +0900

    [Oh] 컨트롤러 오류 수정

--- Back/src/main/java/com/growing/backend/controller/WeatherController.java
+++ Back/src/main/java/com/growing/backend/controller/WeatherController.java
@@ -3,9 +3,8 @@ package com.growing.backend.controller;
 import com.growing.backend.dto.WeatherDataDTO;
 import com.growing.backend.entity.WeatherData;
 import com.growing.backend.entity.WeatherTemperature;
-import com.growing.backend.repository.DateTemperatureRepository;
+import com.growing.backend.repository.WeatherTemperatureRepository;
 import com.growing.backend.repository.WeatherDataRepository;
-import com.growing.backend.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,25 +24,34 @@ import java.util.List;
 @RequestMapping("/weather")
 public class WeatherController {
     private final WeatherDataRepository weatherDataRepository;
-    private final DateTemperatureRepository dateTemperatureRepository;
+    private final WeatherTemperatureRepository weatherTemperatureRepository;
 
     @GetMapping("/all")
     public List<WeatherDataDTO> weather() {
+        // 리스트 형태로 저장
         List<WeatherDataDTO> weatherDataList = new ArrayList<>();
 
-        LocalDate today = LocalDate.now();
+        // 오늘, 내일, 내일 모레 날짜
+        LocalDate today = LocalDate.now(ZoneId.of("Asia/seoul"));
         LocalDate tomorrow = today.plusDays(1);
         LocalDate dayAfterTomorrow = today.plusDays(2);
 
+        // 시간 데이터
+        LocalTime hour = LocalTime.now(ZoneId.of("Asia/seoul")).truncatedTo(ChronoUnit.HOURS);
+
+        // 배열로 저장
         List<LocalDate> dates = Arrays.asList(today, tomorrow, dayAfterTomorrow);
 
+        // 반복문으로 리스트 저장
         for (int i = 0; i < dates.size(); i++) {
             LocalDate date = dates.get(i);
 
-            WeatherData weatherData = weatherDataRepository.findByFcstDate(date)
-                    .orElseThrow(() -> new RuntimeException("Weather data not found for date: " + date));
+            // 날짜, 시간 기준 최고, 최저 기온 탐색
+            WeatherData weatherData = weatherDataRepository.findByFcstDateAndFcstTime(date, hour)
+                    .orElseThrow(() -> new RuntimeException("Weather data not found for date: " + date + hour));
 
-            WeatherTemperature weatherTemperature = dateTemperatureRepository.findByFcstDate(date)
+            // 날짜 기준 최고, 최저 기온 탐색
+            WeatherTemperature weatherTemperature = weatherTemperatureRepository.findByFcstDate(date)
                     .orElseThrow(() -> new RuntimeException("Temperature data not found for date: " + date));
 
             WeatherDataDTO weatherDataDTO = new WeatherDataDTO();
