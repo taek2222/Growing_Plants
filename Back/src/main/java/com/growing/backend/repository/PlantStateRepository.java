4b20df5092249faed51db6177aab28a948385fdc
taek2222 <oot309@daum.net>
2024-05-02 15:16:40 +0900

    [Oh] Repository 어노테이션 설정

--- Back/src/main/java/com/growing/backend/repository/PlantStateRepository.java
+++ Back/src/main/java/com/growing/backend/repository/PlantStateRepository.java
@@ -3,9 +3,11 @@ package com.growing.backend.repository;
 import com.growing.backend.entity.PlantState;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PlantStateRepository extends JpaRepository<PlantState, Integer> {
     List<PlantState> findAllByOrderByDateDescTimeDesc(Pageable pageable);
 }
