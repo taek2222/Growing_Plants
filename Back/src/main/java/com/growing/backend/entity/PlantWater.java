2f0be6dec4e80c4221ca1941f08304086236e4ec
taek2222 <oot309@daum.net>
2024-05-16 11:28:14 +0900

    [Oh] 물 주기 액션
    Signed-off-by: taek2222 <oot309@daum.net>

---
+++ Back/src/main/java/com/growing/backend/entity/PlantWater.java
@@ -0,0 +1,25 @@
+package com.growing.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PLANT_WATER")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlantWater {
+    @Id
+    @Column(name = "PLANT_ID")
+    private int plantId; // 식물 ID
+
+    @Column(name = "WATER_Toggle")
+    private boolean waterToggle; // 물 주기 옵션 활성화
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "plant_id")
+    private Plant plant;
+}
