c3da327d07df759b4f9b0334906c839f99271e1e
taek2222 <oot309@daum.net>
2024-05-17 14:31:24 +0900

    [Oh] 오타 수정
    Signed-off-by: taek2222 <oot309@daum.net>

--- Back/src/main/java/com/growing/backend/entity/PlantWater.java
+++ Back/src/main/java/com/growing/backend/entity/PlantWater.java
@@ -15,7 +15,7 @@ public class PlantWater {
     @Column(name = "PLANT_ID")
     private int plantId; // 식물 ID
 
-    @Column(name = "WATER_Toggle")
+    @Column(name = "WATER_TOGGLE")
     private boolean waterToggle; // 물 주기 옵션 활성화
 
     @OneToOne
