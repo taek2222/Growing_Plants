8fa86bde336ac58297f35831063de30afe6980ed
taek2222 <oot309@daum.net>
2024-05-19 14:00:06 +0900

    [Oh] DTO 추가
    Signed-off-by: taek2222 <oot309@daum.net>

---
+++ Back/src/main/java/com/growing/backend/dto/response/PlantStateDTO.java
@@ -0,0 +1,18 @@
+package com.growing.backend.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+// 식물 센서 값 POST(Back->Front) 요청 DTO
+public class PlantStateDTO {
+    private double lightIntensity; // 조도
+    private double airTemperature; // 대기 온도
+    private double airHumidity; // 대기 습도
+    private int waterAmount; // 물통의 물 양
+    private double soilMoisture1; // 토양 습도 (식물 1)
+    private double soilMoisture2; // 토양 습도 (식물 2)
+    private boolean lightStatus1; // 식물등 상태 (식물 1)
+    private boolean lightStatus2; // 식물등 상태 (식물 2)
+}
