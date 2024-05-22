84a345a0f54d1e3f6d1279afbb50371b1c31311b
taek2222 <oot309@daum.net>
2024-05-22 16:28:37 +0900

    [Oh] 식물 상세 정보 DTO 이름 변경
    Signed-off-by: taek2222 <oot309@daum.net>

--- Back/src/main/java/com/growing/backend/dto/request/PlantSettingRequest.java
+++ Back/src/main/java/com/growing/backend/dto/request/PlantSettingRequest.java
@@ -8,10 +8,10 @@ import java.time.LocalDate;
 @Getter
 @Setter
 // 식물 상세 정보 PATCH(Front->Back) 요청 DTO
-public class PlantInfoDTO {
-    private int id; // 식물 아이디
-    private String name; // 식물 이름
-    private LocalDate date; // 식물 성장 일자
+public class PlantSettingRequest {
+    private int plantId; // 식물 아이디
+    private String plantName; // 식물 이름
+    private LocalDate date; // 성장 일자
     private double lightThreshold; // 조도 기준치
     private double soilThreshold; // 습도 기준치
     private double waterThreshold; // 물 기준치
