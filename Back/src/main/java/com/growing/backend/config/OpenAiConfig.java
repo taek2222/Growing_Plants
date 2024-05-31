a5b95c40be156ac4c9ee39229fca145c79f6e24a
taek2222 <oot309@daum.net>
2024-05-31 17:51:44 +0900

    webClient Bean Config 생성
    Signed-off-by: taek2222 <oot309@daum.net>

---
+++ Back/src/main/java/com/growing/backend/config/OpenAiConfig.java
@@ -0,0 +1,20 @@
+package com.growing.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class OpenAiConfig {
+    @Value("${openai.api.key}")
+    private String openAiKey;
+
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .defaultHeader("Authorization", "Bearer " + openAiKey)
+                .build();
+    }
+}
